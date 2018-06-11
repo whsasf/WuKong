1ba26e17bd56780560fa0bf9f1751b6f5d5f2680
whsasf <whsasf@126.com>
2018-06-11 23:32:35 +0800

    new

--- lib/python/smtp_operations.py
+++ lib/python/smtp_operations.py
@@ -3,3 +3,17 @@
 
 # import need moduels
 
+from smtplib import SMTP
+
+class SMTP_OPs(SMTP):
+    """define a SMTP class and related functions"""
+    
+    def __init__(self,smtphost, smtpport, local_hostname=None,source_address=None)
+    """initial class"""
+    
+    self.smtphost = smtphost
+    self.smtpport = smtpport
+    self.local_hostname = local_hostname
+    self.source_address = source_address
+    
+    
