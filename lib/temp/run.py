c0e862ac9fc8c6c67dc84870c440a2383094db52
whsasf <whsasf@126.com>
2018-06-11 16:33:18 +0800

    new

--- lib/temp/run.py
+++ lib/temp/run.py
@@ -3,11 +3,11 @@
 
 ##steps:
 # (1) imap login 5 accounts with correct credentials, the other 5 use wrong credentials
-# (2) check and analyze imapserv.stat file .make sure the total attempts are 20 ,and half passed ,half failed
+# (2) check and analyze popserv.stat file .make sure the total attempts are 20 ,and half passed ,half failed
 
 import basic_function
 import basic_class
-import imap_operations
+import pop_operations
 import global_variables
 import time
 import remote_operations
@@ -15,39 +15,35 @@ import stat_statistics
 
 
 #step 1
-basic_class.mylogger_record.info('step1:imap auth_crammd5:10 account with correct passwd, the other 5 use wrong pssswd')
+basic_class.mylogger_record.info('step1:imap login:5 account with correct passwd, the other 5 use wrong pssswd')
 
-mx2_imap1_host_ip,mx2_mss1_host_ip,mx2_mta1_port,mx2_mta1_host_ip,mx2_host1_ip,mx2_pop1_port,mx2_pop1_host,mx2_imap1_port,mx2_imap1_host,mx1_mss1_host_ip,mx1_mss2_host_ip,mx1_imap1_host_ip,mx1_imap1_port,mx1_mta1_host_ip,mx1_mta1_port,mx1_pop1_host_ip,mx1_pop1_port,mx_account,mx1_host1_ip,root_account,root_passwd,test_account_base,default_domain = \
-global_variables.get_values('mx2_imap1_host_ip','mx2_mss1_host_ip','mx2_mta1_port','mx2_mta1_host_ip','mx2_host1_ip','mx2_pop1_port','mx2_pop1_host','mx2_imap1_port','mx2_imap1_host','mx1_mss1_host_ip','mx1_mss2_host_ip','mx1_imap1_host_ip','mx1_imap1_port','mx1_mta1_host_ip','mx1_mta1_port','mx1_pop1_host_ip','mx1_pop1_port','mx_account','mx1_host1_ip','root_account','root_passwd','test_account_base','default_domain')
+mx1_pop1_host_ip,mx1_pop1_port,mx_account,mx1_host1_ip,root_account,root_passwd,test_account_base,default_domain = \
+global_variables.get_values('mx1_pop1_host_ip','mx1_pop1_port','mx_account','mx1_host1_ip','root_account','root_passwd','test_account_base','default_domain')
 
 
 for i in range(1,6): 
-    mximap1 = imap_operations.IMAP_Ops(mx1_imap1_host_ip,mx1_imap1_port)
+    mxpop1 = pop_operations.POP_Ops(mx1_pop1_host_ip,mx1_pop1_port)
     try:
-        mximap1.imap_auth_crammd5(test_account_base+str(i),test_account_base+str(i)) # using correct passwd
-        basic_class.mylogger_record.info('auth_cram-md5 success')
+        mxpop1.pop_apop(test_account_base+str(i),test_account_base+str(i)) # using correct passwd
+        basic_class.mylogger_record.info('apop auth success')
     except:
-        basic_class.mylogger_record.error('auth_cram-md5 fail')
-    mximap1.imap_select()
-    mximap1.imap_logout()
+        basic_class.mylogger_record.error('apop auth fail')
+    mxpop1.pop_stat()
+    mxpop1.pop_quit()
 
 for i in range(6,11): 
-    mximap2 = imap_operations.IMAP_Ops(mx1_imap1_host_ip,mx1_imap1_port)
+    mxpop2 = pop_operations.POP_Ops(mx1_pop1_host_ip,mx1_pop1_port)
     try:
-        mximap2.imap_auth_crammd5(test_account_base+str(i),'password') # using wrong passwd :password here
+        mxpop2.pop_apop(test_account_base+str(i),'password') # using wrong passwd :password here
     except:
-        basic_class.mylogger_record.error('auth_cram-md5 fail')
-    mximap2.imap_logout()
+        basic_class.mylogger_record.error('apop auth fail')
+    mxpop2.pop_quit()
     
 #step 2
-basic_class.mylogger_record.info('fetching imapserv.stat ...')
+basic_class.mylogger_record.info('fetching popserv.stat ...')
 time.sleep (50)
-basic_class.mylogger_record.info('step2:check and analyze imapserv.stat file ...')
-imapserv_stat_content1 = remote_operations.remote_operation(mx1_host1_ip,root_account,root_passwd,'su - {0} -c "cat log/imapserv.stat|grep StatImapAuthCommand"'.format(mx_account),0)
-result_lists1 = stat_statistics.stat_statistic(imapserv_stat_content1,'[0]','StatImapAuthCommand',10)
+basic_class.mylogger_record.info('step2:check and analyze popserv.stat file ...')
+popserv_stat_content = remote_operations.remote_operation(mx1_host1_ip,root_account,root_passwd,'su - {0} -c "cat log/popserv.stat|grep StatPopPassCommand"'.format(mx_account),0)
+result_lists = stat_statistics.stat_statistic(popserv_stat_content,'[200]','StatPopPassCommand',10)
 
-imapserv_stat_content2 = remote_operations.remote_operation(mx2_host1_ip,root_account,root_passwd,'su - {0} -c "cat log/imapserv.stat|grep StatImapAuthCommand"'.format(mx_account),0)
-result_lists2 = stat_statistics.stat_statistic(imapserv_stat_content2,'[0]','StatImapAuthCommand',5)
-
-result_lists = result_lists1 + result_lists2
 basic_function.summary(result_lists)
--- lib/temp/teardown.py
+++ lib/temp/teardown.py
@@ -5,9 +5,12 @@
 # (1) delete accounts :testuser1@openwave.com - testuser10@openwave.com
 # (2) restore the config keys:
 #               /*/common/perfStatThresholds:[]
-#               /*/common/reportParamsInterval: [60]  # default 60
-#               /*/common/badPasswordDelay: [1]       # nodelay ,default 1
-#               /*/common/maxBadPasswordDelay: [90]    # no delay,default 90 
+#               /*/common/reportParamsInterval: [60]      # default 60
+#               /*/common/badPasswordDelay: [1]           # nodelay ,default 1
+#               /*/common/maxBadPasswordDelay: [90]       # no delay,default 90 
+#               /*/popserv/allowAPOP: [false]             # enable apop ,default false
+#               /*/mxos/defaultPasswordStoreType:[sha512] # default is sha512, for apop must use clear
+#               /*/mxos/trustedClient: [false]            # to avoid use full account name(with domain) in apop auth,default is false
 
 import basic_function
 import basic_class
@@ -15,14 +18,13 @@ import imap_operations
 import global_variables
 import remote_operations
 
-mx2_imap1_host_ip,mx2_mss1_host_ip,mx2_mta1_port,mx2_mta1_host_ip,mx2_host1_ip,mx2_pop1_port,mx2_pop1_host,mx2_imap1_port,mx2_imap1_host,mx1_mss1_host_ip,mx1_mss2_host_ip,mx1_imap1_host_ip,mx1_imap1_port,mx1_mta1_host_ip,mx1_mta1_port,mx1_pop1_host_ip,mx1_pop1_port,mx_account,mx1_host1_ip,root_account,root_passwd,test_account_base,default_domain = \
-global_variables.get_values('mx2_imap1_host_ip','mx2_mss1_host_ip','mx2_mta1_port','mx2_mta1_host_ip','mx2_host1_ip','mx2_pop1_port','mx2_pop1_host','mx2_imap1_port','mx2_imap1_host','mx1_mss1_host_ip','mx1_mss2_host_ip','mx1_imap1_host_ip','mx1_imap1_port','mx1_mta1_host_ip','mx1_mta1_port','mx1_pop1_host_ip','mx1_pop1_port','mx_account','mx1_host1_ip','root_account','root_passwd','test_account_base','default_domain')
+mx1_pop1_host,mx1_pop1_port,mx_account,mx1_host1_ip,root_account,root_passwd,test_account_base,default_domain = \
+global_variables.get_values('mx1_pop1_host','mx1_pop1_port','mx_account','mx1_host1_ip','root_account','root_passwd','test_account_base','default_domain')
 
 
 basic_class.mylogger_record.info('step1:delete 10 accounts')
 remote_operations.remote_operation(mx1_host1_ip,root_account,root_passwd,'su - {0} -c \'for ((i=1;i<=10;i++));do account-delete {1}$i@{2};done\''.format(mx_account,test_account_base,default_domain),1,'Mailbox Deleted Successfully',10)
-remote_operations.remote_operation(mx2_host1_ip,root_account,root_passwd,'su - {0} -c \'for ((i=1;i<=10;i++));do account-delete {1}$i@{2};done\''.format(mx_account,test_account_base,default_domain),1,'Mailbox Deleted Successfully',10)
 
 basic_class.mylogger_record.info('step2:restore config keys')
-remote_operations.remote_operation(mx1_host1_ip,root_account,root_passwd,'su - {0} -c \'imconfcontrol -install -key \"/*/common/perfStatThresholds=\";imconfcontrol -install -key \"/*/common/reportParamsInterval=60\";imconfcontrol -install -key \"/*/common/badPasswordDelay=1\";imconfcontrol -install -key \"/*/common/maxBadPasswordDelay=90\";imconfcontrol -install -key \"/*/imapserv/imapProxyHost=\";imconfcontrol -install -key \"/*/popserv/popProxyPort\";imconfcontrol -install -key \"/*/popserv/allowAPOP=false\";imconfcontrol -install -key \"/*/mxos/defaultPasswordStoreType=sha512\";imconfcontrol -install -key \"/*/mxos/trustedClient=false\"\''.format(mx_account),0)
 remote_operations.remote_operation(mx1_host1_ip,root_account,root_passwd,'su - {0} -c \'imconfcontrol -install -key \"/*/common/perfStatThresholds=\";imconfcontrol -install -key \"/*/common/reportParamsInterval=60\";imconfcontrol -install -key \"/*/common/badPasswordDelay=1\";imconfcontrol -install -key \"/*/common/maxBadPasswordDelay=90\";imconfcontrol -install -key \"/*/popserv/allowAPOP=false\";imconfcontrol -install -key \"/*/mxos/defaultPasswordStoreType=sha512\";imconfcontrol -install -key \"/*/mxos/trustedClient=false\"\''.format(mx_account),0)
+
