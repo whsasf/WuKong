83f8272e6ebefcc51b7c45ec3912699122bbaca8
whsasf <whsasf@126.com>
2018-06-12 23:18:53 +0800

    new

--- test_cases/cassandra_test/cassandra_test.py
+++ test_cases/cassandra_test/cassandra_test.py
@@ -8,7 +8,7 @@ cluster = Cluster(['10.49.58.240'],port=9043)
 session = cluster.connect('KeyspaceBlobStore')
 
 for i in range(0,20):
-    target = 'select * from "KeyspaceBlobStore"."CF_Message_{}" where key=0xe61a14066e0f11e8bd5fbe29141ea2c6;'.format(i)
+    target = 'select * from "CF_Message_{}" where key=0x93ddff266e4711e8b61ea12237d9bced and column1>=101;'.format(i)
     print(target)
     rows = session.execute(target)     
     print(list(rows))
